e0b7f088df96c2a5563e47d7ea6d338069465810
rdtrimble <r.d.trimble@gmail.com>
2017-01-26 14:32:11 +0000

    Update _deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -8,3 +8,6 @@ git config --global user.email "r.d.trimble@gmail.com"
 git config --global user.name "Richard Trimble"
 
 cd /docs
+git add .
+git commit -m 'update docs' || true
+git push -q origin master
